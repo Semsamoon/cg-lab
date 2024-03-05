36227eb621ec04e2ec63fcf97c7cbc17d03d9ee1
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2024-03-06 00:42:11 +0300

    Add Transform

---
+++ Engine/Transform/TransformComponent.cpp
@@ -0,0 +1,48 @@
+﻿#include "TransformComponent.h"
+
+using namespace engine::transform;
+
+TransformComponent::TransformComponent()
+{
+    scale_ = float3(1);
+    UpdateWorldMatrix();
+}
+
+void TransformComponent::Compose(TransformComponent* parent)
+{
+    parent_ = parent;
+}
+
+void TransformComponent::UpdateWorldMatrix()
+{
+    world_matrix_ =
+        float4x4::CreateScale(scale_)
+        * float4x4::CreateFromYawPitchRoll(rotation_)
+        * float4x4::CreateTranslation(position_)
+        * (parent_ == nullptr ? float4x4::Identity : parent_->world_matrix_);
+}
+
+TransformComponent* TransformComponent::parent() const
+{
+    return parent_;
+}
+
+float4x4& TransformComponent::world_matrix()
+{
+    return world_matrix_;
+}
+
+float3& TransformComponent::position()
+{
+    return position_;
+}
+
+float3& TransformComponent::rotation()
+{
+    return rotation_;
+}
+
+float3& TransformComponent::scale()
+{
+    return scale_;
+}
---
+++ Engine/Transform/TransformComponent.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "../../Lib/Types.h"
+
+namespace engine
+{
+    namespace transform
+    {
+        class TransformComponent
+        {
+        public:
+            TransformComponent();
+
+            void Compose(TransformComponent* parent);
+            void UpdateWorldMatrix();
+
+            TransformComponent* parent() const;
+            
+            float3& position();
+            float3& rotation();
+            float3& scale();
+            float4x4& world_matrix();
+
+        private:
+            TransformComponent* parent_ = nullptr;
+
+            float3 position_;
+            float3 rotation_;
+            float3 scale_;
+            float4x4 world_matrix_;
+        };
+    }
+}
