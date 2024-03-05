f9d5b37347cbafdc9dfb8081e9cd89b82303b5a0
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2024-03-06 00:43:53 +0300

    Add Cube

--- Shaders/Shader.hlsl
+++ Shaders/Shader.hlsl
@@ -1,38 +1,32 @@
 struct VS_IN
 {
-	float4 pos : POSITION0;
-	float4 col : COLOR0;
+    float4 pos : POSITION0;
+    float4 col : COLOR0;
 };
 
 struct PS_IN
 {
-	float4 pos : SV_POSITION;
- 	float4 col : COLOR;
+    float4 pos : SV_POSITION;
+    float4 col : COLOR;
 };
 
-struct Addition
+cbuffer TransformBuffer : register(b0)
 {
-	float4 offset;
-	float4 color;
-};
-
-cbuffer AdditionBuffer : register(b0)
-{
-	Addition AdditionData;
+float4x4 Transform;
 }
 
-PS_IN VSMain( VS_IN input )
+PS_IN VSMain(VS_IN input)
 {
-	PS_IN output;
-	
-	output.pos = float4(input.pos.xyz + AdditionData.offset.xyz, 1);
-	output.col = AdditionData.color;
-	
-	return output;
+    PS_IN output;
+
+    output.pos = mul(float4(input.pos.xyz, 1), Transform);
+    output.col = input.col;
+
+    return output;
 }
 
-float4 PSMain( PS_IN input ) : SV_Target
+float4 PSMain(PS_IN input) : SV_Target
 {
-	float4 col = input.col;
-	return col;
+    float4 col = input.col;
+    return col;
 }
